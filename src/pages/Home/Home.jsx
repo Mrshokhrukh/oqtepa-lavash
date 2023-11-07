a4d0de532bffbf311e56faf80b3edeba1d859649
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-11-07 23:46:09 +0500

    az

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Nav_catalogs from "../../components/nav_catalogs/Nav_catalogs";
 import menu from "./home.module.scss";
 import { BiCategoryAlt } from "react-icons/bi";
+import { BsHeart } from "react-icons/bs";
 import { catalogs } from "../../assets/data";
 const Home = () => {
   return (
@@ -32,8 +33,117 @@ const Home = () => {
           </div>
         </div>
         <div className={menu.manu_products}>
-          <h1>category title</h1>
-          <div className={menu.menu_products_right}></div>
+          <h1 className={menu.product_title}>Lavash</h1>
+          <div className={menu.menu_products_right}>
+            <div className={menu.card}>
+              <div className={menu.like}>
+                <span>
+                  <BsHeart />
+                </span>
+              </div>
+              <img
+                src="http://cc.oqtepalavash.uz/api/image/33f5cfcb-0071-4f1a-b1f1-22dc1de28f3b.jpg"
+                alt=""
+              />
+              <div className={menu.card_info}>
+                <p className={menu.name}>lavash</p>
+                <p className={menu.price}>
+                  28 000 <span>sum</span>
+                </p>
+                <button className={menu.button}>Add</button>
+              </div>
+            </div>
+            <div className={menu.card}>
+              <div className={menu.like}>
+                <span>
+                  <BsHeart />
+                </span>
+              </div>
+              <img
+                src="http://cc.oqtepalavash.uz/api/image/33f5cfcb-0071-4f1a-b1f1-22dc1de28f3b.jpg"
+                alt=""
+              />
+              <div className={menu.card_info}>
+                <p className={menu.name}>lavash</p>
+                <p className={menu.price}>
+                  28 000 <span>sum</span>
+                </p>
+                <button className={menu.button}>Add</button>
+              </div>
+            </div>
+            <div className={menu.card}>
+              <div className={menu.like}>
+                <span>
+                  <BsHeart />
+                </span>
+              </div>
+              <img
+                src="http://cc.oqtepalavash.uz/api/image/33f5cfcb-0071-4f1a-b1f1-22dc1de28f3b.jpg"
+                alt=""
+              />
+              <div className={menu.card_info}>
+                <p className={menu.name}>lavash</p>
+                <p className={menu.price}>
+                  28 000 <span>sum</span>
+                </p>
+                <button className={menu.button}>Add</button>
+              </div>
+            </div>
+            <div className={menu.card}>
+              <div className={menu.like}>
+                <span>
+                  <BsHeart />
+                </span>
+              </div>
+              <img
+                src="http://cc.oqtepalavash.uz/api/image/33f5cfcb-0071-4f1a-b1f1-22dc1de28f3b.jpg"
+                alt=""
+              />
+              <div className={menu.card_info}>
+                <p className={menu.name}>lavash</p>
+                <p className={menu.price}>
+                  28 000 <span>sum</span>
+                </p>
+                <button className={menu.button}>Add</button>
+              </div>
+            </div>
+            <div className={menu.card}>
+              <div className={menu.like}>
+                <span>
+                  <BsHeart />
+                </span>
+              </div>
+              <img
+                src="http://cc.oqtepalavash.uz/api/image/33f5cfcb-0071-4f1a-b1f1-22dc1de28f3b.jpg"
+                alt=""
+              />
+              <div className={menu.card_info}>
+                <p className={menu.name}>lavash</p>
+                <p className={menu.price}>
+                  28 000 <span>sum</span>
+                </p>
+                <button className={menu.button}>Add</button>
+              </div>
+            </div>
+            <div className={menu.card}>
+              <div className={menu.like}>
+                <span>
+                  <BsHeart />
+                </span>
+              </div>
+              <img
+                src="http://cc.oqtepalavash.uz/api/image/33f5cfcb-0071-4f1a-b1f1-22dc1de28f3b.jpg"
+                alt=""
+              />
+              <div className={menu.card_info}>
+                <p className={menu.name}>lavash</p>
+                <p className={menu.price}>
+                  28 000 <span>sum</span>
+                </p>
+                <button className={menu.button}>Add</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
